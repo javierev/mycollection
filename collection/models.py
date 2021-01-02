64e3fecc4b283d181199d3a8ca7c3b10f66b62fb
Javier Estevez <jestevez@valdaris.com>
2021-01-02 02:11:48 +0100

    Added year in console model

--- collection/models.py
+++ collection/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Console(models.Model):
     name = models.CharField(max_length=100)
     short_name = models.CharField(max_length=10)
+    year = models.IntegerField(default=1999)
 
 class Game(models.Model):
     console = models.ForeignKey(Console, on_delete=models.DO_NOTHING)
