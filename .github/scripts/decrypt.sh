5553fea40434948459c2c9577422ae0e75b918e6
Javier Estevez <jestevez@valdaris.com>
2021-01-04 03:27:22 +0100

    Fixed paths in decrypt.sh

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_KEY" \
---output ../../mycollection/.env ../../mycollection/.env.gpg
+--output ./mycollection/.env ./mycollection/.env.gpg
