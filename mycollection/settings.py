f29b26be50c1759c9df9ae45079a25f73228ca21
Javier Estevez <jestevez@valdaris.com>
2021-01-04 14:25:05 +0100

    Fix database configuration

--- mycollection/settings.py
+++ mycollection/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': config('DATABASE_ENGINE'),
         'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
 
