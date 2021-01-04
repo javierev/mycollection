55d2bd75cd02544e1947f869b00ab58d5fb4ae34
Javier Estevez <jestevez@valdaris.com>
2021-01-04 03:25:53 +0100

    Fixed typo in decrypt.sh

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRPYT_KEY" \
+gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_KEY" \
 --output ../../mycollection/.env ../../mycollection/.env.gpg
